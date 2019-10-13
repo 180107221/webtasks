7bd66d35165a99895b9e482b1b1388ee5248407b
Magzhan <kojashmet.magjan@gmail.com>
2019-10-13 13:27:10 +0600

    First commit

---
+++ Project/database.php
@@ -0,0 +1,13 @@
+<?php
+$name = $__GET['name'];
+$lastname = $__GET['lastname'];
+$year = $__GET['year'];
+$email = $__GET['email'];
+$password = $__GET['password'];
+$password1 = $__GET['password1'];
+
+if ($password == $password1) {
+	echo "<a href="Webpage.html">";
+}
+
+?>
